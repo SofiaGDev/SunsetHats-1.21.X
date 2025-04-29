8eee1e6223ccd8c1cacd090f92e0e089025d0138
Sofia Gomes <sofia811801@gmail.com>
2025-04-29 20:51:44 -0300

    Initial commit

---
+++ src/main/java/net/sofiazeda/sunsethats/SunsetHatsClient.java
@@ -0,0 +1,10 @@
+package net.sofiazeda.sunsethats;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SunsetHatsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sofiazeda/sunsethats/SunsetsHats.java
@@ -0,0 +1,16 @@
+package net.sofiazeda.sunsethats;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SunsetsHats implements ModInitializer {
+
+	public static final String MOD_ID = "sunsetshats";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
